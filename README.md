ea0ba8a232dba67936b3e3a53042b30562ffc85b
Scottlexium <osarumenscott9@gmail.com>
2021-02-05 17:51:55 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-mach001
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-mach001)
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./style.css";
+
+export default function App() {
+  return (
+    <div>
+      <h1>Welcome to Mach001!</h1>
+      <p>Starting prototype Mach001 :)</p>
+    </div>
+  );
+}
